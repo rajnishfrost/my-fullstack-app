1519caf2f52cc465c7353e208e04b3068c1f0866
rajnishfrost <rajnishfrost@gmail.com>
2024-06-26 20:14:16 +0530

    9

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 var cors = require("cors");
 require('dotenv').config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
